7837758a1ccf9aae99642c68f3d1138c7033baed
lverghese <loganverghese@gmail.com>
2021-08-13 00:06:48 -0400

    generate

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-
-const printProfileData = profileDataArr => {
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-
-  };
-
-printProfileData(profileDataArgs);
-=======
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
@@ -24,4 +12,3 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
->>>>>>> feature/generate-webpage
