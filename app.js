3ac9f3c94e94a3625b99a9372e3342d8955e52b3
lverghese <loganverghese@gmail.com>
2021-08-15 22:40:36 -0400

    multiple objects

--- app.js
+++ app.js
@@ -23,7 +23,12 @@ const promptUser = () => {
 };
 
 
-const promptProject = () => {
+const promptProject = portfolioData => {
+
+ // If there's no 'projects' array property, create one
+if (!portfolioData.projects) {
+  portfolioData.projects = [];
+}
   console.log(`
 =================
 Add a New Project
@@ -63,14 +68,23 @@ Add a New Project
       message: 'Would you like to enter another project?',
       default: false
     }
-  ]);
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
 };
 
 
 promptUser()
-  .then(answers => console.log(answers))
   .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
